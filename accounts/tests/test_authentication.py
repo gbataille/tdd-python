9dcd5991b3a907a2bd0595bcc71da217f9a835cf
Grégory Bataille <gregory.bataille@gmail.com>
2016-04-03 18:01:43 +0200

    backend

--- accounts/tests/test_authentication.py
+++ accounts/tests/test_authentication.py
@@ -45,3 +45,33 @@ class AuthenticationTest(TestCase):
                 }
         found_user = self.backend.authenticate('an assertion')
         self.assertEqual(user, found_user)
+
+    def test_creates_new_user_if_necessary_for_valid_assertion(
+            self, mock_post):
+        mock_post.return_value.json.return_value = {
+                'status': 'okay',
+                'email': 'new@user.com'
+                }
+        found_user = self.backend.authenticate('an assertion')
+        new_user = User.objects.get(email='new@user.com')
+        self.assertEqual(found_user, new_user)
+
+
+class GetUserTest(TestCase):
+
+    def test_gets_user_by_email(self):
+        backend = PersonaAuthenticationBackend()
+        other_user = User(email='other@user.com')
+        other_user.username = 'otheruser'
+        other_user.save()
+        desired_user = User.objects.create(email='a@b.com')
+        found_user = backend.get_user('a@b.com')
+        self.assertEqual(found_user, desired_user)
+
+    def test_returns_none_if_no_user_with_email(self):
+        backend = PersonaAuthenticationBackend()
+        other_user = User(email='other@user.com')
+        other_user.username = 'otheruser'
+        other_user.save()
+        found_user = backend.get_user('a@b.com')
+        self.assertIsNone(found_user)
