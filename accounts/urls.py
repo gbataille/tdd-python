bfb2a8ac62c524555e0d0d699a730f97f692c3c8
Grégory Bataille <gregory.bataille@gmail.com>
2016-04-05 06:53:34 +0200

    logout

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,4 +4,6 @@ from accounts import views
 
 urlpatterns = [
         url(r'^login$', views.persona_login, name='persona_login'),
+        url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page':
+            '/'}, name='logout'),
         ]
